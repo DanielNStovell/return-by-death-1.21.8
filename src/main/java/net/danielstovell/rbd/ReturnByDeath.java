a0a24b92ac4f1448eaa98aeba63896af90b897e2
Daniel Stovell <danielnstovell@gmail.com>
2025-07-19 13:04:09 +0100

    Initial commit

---
+++ src/main/java/net/danielstovell/rbd/ReturnByDeath.java
@@ -0,0 +1,16 @@
+package net.danielstovell.rbd;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReturnByDeath implements ModInitializer {
+	public static final String MOD_ID = "return-by-death";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/danielstovell/rbd/ReturnByDeathClient.java
@@ -0,0 +1,11 @@
+package net.danielstovell.rbd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReturnByDeathClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
